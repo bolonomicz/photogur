741efcff3c9fef8823b19e4dbe226de85c9a8d59
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-03 09:25:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = 'fa6b979ba6f6dcd82af34b5c0ff376481a9166248492446be21aedc9b6b249a4134ef329a8c814f3a544efc7d71ee4c9ebf97215592cd433736eaffd9c1a1aeb'
