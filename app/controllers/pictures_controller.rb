7030c081b0b3b5326cf7d1e6bdc338ac859a6d59
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-03 09:44:43 -0400

    making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,23 @@
 class PicturesController < ApplicationController
 
 	def index
+		@pictures = [
+			{
+				:title => "The old church on the coast of WHite sea",
+				:artist => "Segey Ershov",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			},
+			{
+				:title => "Sea Power",
+				:artist => "Stephen Scullion",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+			},
+			{
+				:title => "Into te Poppies",
+				:artist => "John Wilhelm",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+			}
+		]
 		
 	end
 end
