e8249948f1606daaec2d832a19841ce368af6966
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-03 12:26:14 -0400

    one can now see the pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,11 +13,37 @@ class PicturesController < ApplicationController
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
 			},
 			{
-				:title => "Into te Poppies",
+				:title => "Into the Poppies",
 				:artist => "John Wilhelm",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
 			}
 		]
+	end
+
+	def picture0
+		@picture = {
+			:title => "The old church on the coast of White sea",
+			:artist => "Sergey Eshov",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+		}
+	end
+
+	def picture1
+		@picture = {
+			:title => "Sea Power",
+			:artist => "Stephen Scullion",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+		}
+	end
+
+	def picture2
+		@picture = {
+			:title => "Into the Poppies",
+			:artist => "John Wilhelm",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+
+		}
 		
 	end
+
 end
