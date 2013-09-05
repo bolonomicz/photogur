28ea35f8c5193e9ab810bc19ab6fae821788b332
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-05 13:17:36 -0400

    fixed name bug

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   attr_accessible :artist, :title, :url
 
-  validates :name,  :presence => true
+  #validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
  
