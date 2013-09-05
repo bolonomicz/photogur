00656c111254948985cbdfcd621fce27b867a5ee
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-05 12:49:58 -0400

    made comments look better

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,15 @@ class CommentsController < ApplicationController
 	def create
     @picture = Picture.find(params[:picture_id])
     @comment = @picture.comments.create(params[:comment])
-    redirect_to pictures_url
+    redirect_to picture_path(@picture)
   end
+
+  def destroy
+    @picture = Picture.find(params[:picture_id])
+    @comment = @picture.comments.find(params[:id])
+    @comment.destroy
+    redirect_to picture_path(@picture)
+  end
+
 
 end
